997c7a72d66281ffd35114a099f0978e5156dfcc
rasclass <rascal.class.1985@gmail.com>
2020-09-08 08:45:30 +0900

    スタイルの設定

--- src/index.js
+++ src/index.js
@@ -4,11 +4,11 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-const url = 'https://wings.msn.to'
-  ReactDOM.render(
-    <a href={url}>サポートサイト</a>,
-    document.getElementById('root')
-  );  
+const style = {color:'Red', backgroundColor:'Yellow'};
+ReactDOM.render(
+  <p style={style}>WINGSプロジェクト</p>,
+  document.getElementById('root')
+);  
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
