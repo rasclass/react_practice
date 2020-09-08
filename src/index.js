f39b66c411738abf87a15959f687026e60352024
rasclass <rascal.class.1985@gmail.com>
2020-09-08 09:12:17 +0900

    様々な値を渡す

--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import MyHello from './MyHello'
+import MyType from './MyType'
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <MyHello name="山田"/>,
+  <div>
+    <MyType value="山田" />
+    <MyType value={'鈴木'} />
+    <MyType value={108} />
+    <MyType value={true} />
+    <MyType value={['うめ','もも', 'さくら']} />
+    <MyType value={{name:"山田太郎", age:40}} />
+    <MyType value={() => console.log("Hoge")} />
+  </div>,
   document.getElementById('root')
 );  
 
