1b25555bf62aae0947b0a8c74f29b7493837a46f
rasclass <rascal.class.1985@gmail.com>
2020-09-10 11:30:24 +0900

    イベント処理2

--- src/MyEvent.js
+++ src/MyEvent.js
@@ -1,6 +1,11 @@
 import React, {Component} from 'react';
 
 export default class MyEvent extends Component{
+    constructor(props){
+        super(props)
+        this.show = this.show.bind(this);
+    }
+
     show(e){
         console.log(`${this.props.greet}, ${e.target.value}!!`);
     }
@@ -9,7 +14,7 @@ export default class MyEvent extends Component{
         return(
             <form>
                 <label htmlFor="txtName">名前：</label>
-                <input id="txtName" type="text" onChange={this.show.bind(this)} />
+                <input id="txtName" type="text" onChange={this.show} />
             </form>
         )
     }
