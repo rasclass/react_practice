32dccb3dee85cad8a07275281edce88db39208b5
rasclass <rascal.class.1985@gmail.com>
2020-09-10 11:59:19 +0900

    親子間でのデータの引き渡し

--- src/index.js
+++ src/index.js
@@ -2,10 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
-import MyState from './MyState'
+import MyParent from './MyParent'
 
 ReactDOM.render(
-  <MyState />,
+  <MyParent />,
   document.getElementById('root')
 );  
 
