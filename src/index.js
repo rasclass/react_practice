0065d50af256bfb748e516b95bdd1c20fba4d839
rasclass <rascal.class.1985@gmail.com>
2020-09-10 11:12:03 +0900

    条件分岐1

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import MyHello from './MyHello'
 import * as serviceWorker from './serviceWorker';
 import MyArticle from './MyArticle';
 
