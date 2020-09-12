62556c11277a1957f9460899597241a524e3b7b5
rasclass <rascal.class.1985@gmail.com>
2020-09-12 16:45:41 +0900

    選択ボックス

--- src/index.js
+++ src/index.js
@@ -2,10 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
-import MyTextarea from './MyTextarea'
+import MySelect from './MySelect'
 
 ReactDOM.render(
-  <MyTextarea />,
+  <MySelect />,
   document.getElementById('root')
 );  
 
